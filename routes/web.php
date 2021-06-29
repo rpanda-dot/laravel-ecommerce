b8450cc3d27732bd1a0f8de08a14c5f57970d583
Rajaram Panda <rpanda@argusoft.in>
2021-06-29 16:39:03 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix'=>'admin','middleware'=>['admin:admin']],function(){
+Route::get('/login',[AdminController::class,'loginForm']);
+Route::get('/login',[AdminController::class,'store'])->name('amdin.login');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
